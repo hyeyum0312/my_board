8b03884d078af8c77fad57929ac1405769e8270c
hyeyum0312 <hyeyum0312@gmail.com>
2024-12-27 00:26:14 +0900

    feat: dotenv install

--- src/lib/msw/utils.tsx
+++ src/lib/msw/utils.tsx
@@ -1,4 +1,5 @@
-import { HttpResponseResolver, http } from 'msw';
+import { rest } from 'msw'; // 'rest'로 변경
+import { HttpResponseResolver } from 'msw';
 
 export const makeHandler = ({
   path,
@@ -7,7 +8,7 @@ export const makeHandler = ({
 }: {
   path: string;
   method?: 'get' | 'post' | 'put' | 'delete' | 'patch' | 'head' | 'options';
-  resolver: HttpResponseResolver;
+  resolver: HttpResponseResolver; // 'HttpResponseResolver'는 올바르게 타입이 지정됨
 }) => {
-  return http[method ?? 'get'](path, resolver);
+  return rest[method ?? 'get'](path, resolver); // 'rest'로 변경
 };
