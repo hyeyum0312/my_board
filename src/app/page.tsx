7e60c1b7717ccddad8c04ffd98b37db1855dd6da
hyeyeon <hyeyum0312@github.com>
2024-12-27 10:53:51 +0900

    Refactor: msw, api관련 폴더구조 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-import { getQuestions } from '@/apis/fetchers/questions/get-questions';
+import { getQuestions } from '@/config/apis/fetchers/questions/get-questions';
 
 export default async function Home() {
   const { data } = await getQuestions();
 
   return (
     <div>
-      {Array.isArray(data.questions) ? (
-        data.questions.map((item) => <div key={item.id}>{item.question}</div>)
-      ) : (
-        <div>No questions available.</div>
-      )}
+      <div>No questions available.</div>
     </div>
   );
 }
--- src/instrumentation.tsx
+++
@@ -1,8 +0,0 @@
-// import { ServerEnv } from './constants/server-env';
-
-// export async function register() {
-//   if (process.env.NEXT_RUNTIME === 'nodejs' && ServerEnv.mocking) {
-//     const { initMocks } = await import('./lib/msw/server');
-//     initMocks();
-//   }
-// }
